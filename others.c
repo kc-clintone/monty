0e37277ef704183dad8f80270f82b3955d9a7c3a
kc-clintone <kayseeclintone@gmail.com>
2024-01-30 22:28:46 +0000

    [Making changes >>> 003]

--- others.c
+++ others.c
@@ -22,28 +22,28 @@ free(*stack_p);
  * @cn: The line_number
  * Return: Nada
 */
-void f_add(stack_t **head, unsigned int counter)
+void add_fn(stack_t **hd, unsigned int cn)
 {
-	stack_t *h;
-	int len = 0, aux;
+stack_t *h;
+int len = 0, aux;
 
-	h = *head;
-	while (h)
-	{
-		h = h->next;
-		len++;
-	}
-	if (len < 2)
-	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	aux = h->n + h->next->n;
-	h->next->n = aux;
-	*head = h->next;
-	free(h);
+h = *hd;
+while (h)
+{
+h = h->next;
+len++;
+}
+if (len < 2)
+{
+fprintf(stderr, "L%d: can't add, stack too short\n", cn);
+fclose(bus.file);
+free(bus.content);
+free_stack(*hd);
+exit(EXIT_FAILURE);
+}
+h = *hd;
+aux = h->n + h->next->n;
+h->next->n = aux;
+*hd = h->next;
+free(h);
 }
