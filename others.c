339e41734cd18fc5e552599eb0e3a2c06a462d48
kc-clintone <kayseeclintone@gmail.com>
2024-01-30 22:23:54 +0000

    [Making changes >>> 002]

--- others.c
+++ others.c
@@ -14,3 +14,36 @@ free(*stack_p);
 *stack_p = tmp;
 }
 }
+
+/**
+ * add_fn - A funtion that adds the top two elements of the
+ * stack.
+ * @hd: Head of stack
+ * @cn: The line_number
+ * Return: Nada
+*/
+void f_add(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int len = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	aux = h->n + h->next->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
+}
